29a4cd2773678a215f8fc7d3b7603544e186e96a
Mike McGuire <mcguire@mullenhigh.com>
2014-09-12 10:33:25 -0600

    Buttons lined up properly

--- IJP Lesson 6 - Layout Combo/src/LayoutCombo.java
+++ IJP Lesson 6 - Layout Combo/src/LayoutCombo.java
@@ -5,9 +5,9 @@ import javax.swing.*;
 public class LayoutCombo implements ActionListener
     {
     private JFrame frame;
-    private JPanel panel;
+    private JPanel panelLeft, panelRight;
     private JLabel label;
-    private JButton button1, button2, button3, button4;
+    private JButton button1, button2, button3, button4, button5, button6, button7, button8;
     private JTextArea textArea;
     private JScrollPane scrollArea;
  
@@ -24,25 +24,44 @@ public class LayoutCombo implements ActionListener
         Container contentPane = frame.getContentPane();
         contentPane.setLayout(new BorderLayout());
  
-        label = new JLabel("Click a button to see its text appear below:");
+        label = new JLabel("Use the buttons to control the layout of the text you type");
         contentPane.add(label, BorderLayout.NORTH);
  
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        panelLeft = new JPanel();
+        panelLeft.setLayout(new BoxLayout(panelLeft,BoxLayout.Y_AXIS));
  
         button1 = new JButton("Button 1");
         button1.addActionListener(this);
-        panel.add(button1);
+        panelLeft.add(button1);
         button2 = new JButton("Button 2");
         button2.addActionListener(this);
-        panel.add(button2);
+        panelLeft.add(button2);
         button3 = new JButton("Button 3");
         button3.addActionListener(this);
-        panel.add(button3);
+        panelLeft.add(button3);
         button4 = new JButton("Button 4");
         button4.addActionListener(this);
-        panel.add(button4);
-        contentPane.add(panel, BorderLayout.WEST);
+        panelLeft.add(button4);
+
+        contentPane.add(panelLeft, BorderLayout.WEST);
+        
+        panelRight = new JPanel();
+        panelRight.setLayout(new BoxLayout(panelRight,BoxLayout.Y_AXIS));
+        
+        button5 = new JButton("Button 5");
+        button5.addActionListener(this);
+        panelRight.add(button5);
+        button6 = new JButton("Button 6");
+        button6.addActionListener(this);
+        panelRight.add(button6);
+        button7 = new JButton("Button 7");
+        button7.addActionListener(this);
+        panelRight.add(button7);
+        button8 = new JButton("Button 8");
+        button8.addActionListener(this);
+        panelRight.add(button8);
+       
+        contentPane.add(panelRight, BorderLayout.EAST);
  
         textArea = new JTextArea(10,25);
         scrollArea = new JScrollPane(textArea);
@@ -64,6 +83,14 @@ public class LayoutCombo implements ActionListener
             textArea.append("Button 3 was clicked.\n");
         else if (e.getSource() == button4)
             textArea.append("Button 4 was clicked.\n");
+        else if (e.getSource() == button5)
+            textArea.append("Button 5 was clicked.\n");
+        else if (e.getSource() == button6)
+	        textArea.append("Button 6 was clicked.\n");
+	    else if (e.getSource() == button7)
+	        textArea.append("Button 7 was clicked.\n");
+	    else if (e.getSource() == button8)
+	        textArea.append("Button 8 was clicked.\n");
         else
             textArea.append("Should not get here!\n");
         }
